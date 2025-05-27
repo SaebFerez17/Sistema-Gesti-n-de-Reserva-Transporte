6688df3020a8d27e473d466abf80bb05e995ee44
spico <spico@Sebastian>
2025-05-27 16:55:14 -0500

    modelo, controlador y vista de conductor. solo tiene algnos errores ortograficos en lso sout print

--- src/main/java/com/mycompany/sistema_gestion_reserva_transporte_final/Sistema_Gestion_Reserva_Transporte_Final.java
+++ src/main/java/com/mycompany/sistema_gestion_reserva_transporte_final/Sistema_Gestion_Reserva_Transporte_Final.java
@@ -4,7 +4,9 @@
 
 package com.mycompany.sistema_gestion_reserva_transporte_final;
 
-import Controlador.ControladorUsuario;
+import Controlador.ControladorConductor;
+import Modelo.Conductor;
+import Vista.VistaConductor;
 import Vista.VistaUsuario;
 
 /**
@@ -14,7 +16,9 @@ import Vista.VistaUsuario;
 public class Sistema_Gestion_Reserva_Transporte_Final {
 
     public static void main(String[] args) {
-        ControladorUsuario controlador = new ControladorUsuario(new VistaUsuario());
+        Modelo.Conductor conductor = new Conductor("Manolo",123,2,new String[] {"LUNES","VIERNES","DOMINGO"});
+        System.out.println(conductor.toString());;
+        ControladorConductor controlador = new ControladorConductor(new VistaConductor());
         controlador.Ejecutar();
     }
     
