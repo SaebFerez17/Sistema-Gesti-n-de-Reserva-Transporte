c0831b5acd164bf801d2e01b23f429a69d698484
spico <spico@Sebastian>
2025-05-27 11:07:59 -0500

    conductor

---
+++ src/main/java/Vista/VistaConductor.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Vista;
+
+import Utilidades.Util;
+
+/**
+ *
+ * @author spico
+ */
+public class VistaConductor {
+     public String[] IngresarConductor(){
+        String[] datos = new String[2];
+        datos[0] = String.valueOf(Utilidades.Util.getString("Digite el nombre del conductor: "));
+        datos[1] = String.valueOf(Utilidades.Util.getInt("Digite la cedula del conductor: "));
+        datos[3] =
+        return datos;
+    }
+    
+    public int Menu(){
+        String[] opciones = {"1) Agregar", "2) Boorrar", "3) Actualizar", "4)Listar"};
+        return Util.menu(opciones);
+    }
+    
+    public String getString(String msj){
+        return Util.getString(msj);
+    }
+    public int getInt(String msj){
+        return Util.getInt(msj);
+    }
+    public long getLong(String msj){
+        return Util.getLong(msj);
+    }
+}
