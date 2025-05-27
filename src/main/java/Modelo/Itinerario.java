d0ff9648f3578c79abea86ae931d5e3f30a49dd3
BIBLIOTECA <BIBLIOTECA@DESKTOP-K9OSBM0>
2025-05-27 08:15:39 -0500

    El que hace esto

--- src/main/java/Modelo/Itinerario.java
+++ src/main/java/Modelo/Itinerario.java
@@ -14,4 +14,56 @@ public class Itinerario {
     private String[] Ruta = new String[2];
     private Conductor conductor;
     private double precio;
+
+    public Itinerario() {
+    }
+
+    public Itinerario(String hora, Conductor conductor, double precio) {
+        this.hora = hora;
+        this.conductor = conductor;
+        this.precio = precio;
+    }
+
+    public String getHora() {
+        return hora;
+    }
+
+    public void setHora(String hora) {
+        this.hora = hora;
+    }
+
+    public int[] getFecha() {
+        return Fecha;
+    }
+
+    public void setFecha(int[] Fecha) {
+        this.Fecha = Fecha;
+    }
+
+    public String[] getRuta() {
+        return Ruta;
+    }
+
+    public void setRuta(String[] Ruta) {
+        this.Ruta = Ruta;
+    }
+
+    public Conductor getConductor() {
+        return conductor;
+    }
+
+    public void setConductor(Conductor conductor) {
+        this.conductor = conductor;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+    
+    
+    
 }
