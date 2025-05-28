b1b5de304d5a5e9178e020009c1ec07bdfb0cea8
BIBLIOTECA <BIBLIOTECA@DESKTOP-K9OSBM0>
2025-05-28 12:02:40 -0500

    Esto es Prueba

--- src/main/java/Vista/VistaAutobus.java
+++ src/main/java/Vista/VistaAutobus.java
@@ -4,16 +4,7 @@ package Vista;
 import Utilidades.Util;
 
 
-public class VistaAutobus {
-    public String[] IngresarConductor(){
-        String[] datos = new String[4];
-        datos[0] = String.valueOf(Utilidades.Util.getString("Digite el nombre del conductor: "));
-        datos[1] = String.valueOf(Utilidades.Util.getInt("Digite la cedula del conductor: "));
-        datos[2] = String.valueOf(Utilidades.Util.getInt("Digite el salario del conductor: "));
-        datos[3] = String.valueOf(Utilidades.Util.getString("Igresa los dias a Trabajar separador por ( , ): "));
-        return datos;
-    }
-    
+public class VistaAutobus {    
     public int Menu(){
         String[] opciones = {"1) Agregar", "2) Boorrar", "3) Actualizar", "4)Listar"};
         return Util.menu(opciones);
