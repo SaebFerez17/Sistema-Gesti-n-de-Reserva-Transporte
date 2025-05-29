50f1316e7130dc97398dd5f465aef0bdef4d9578
BIBLIOTECA <BIBLIOTECA@DESKTOP-K9OSBM0>
2025-05-29 12:48:06 -0500

    Merge origin/master

--- src/main/java/Controlador/ControladorAutobus.java
+++ src/main/java/Controlador/ControladorAutobus.java
@@ -2,13 +2,14 @@
 package Controlador;
 
 import Modelo.Autobuses;
+import Modelo.Usuario;
 import Vista.VistaAutobus;
 import java.util.ArrayList;
 
-public class ControladorAutobus{
+public class ControladorAutobus implements IMETODOSCRUD<Autobuses,String>{
     
     private VistaAutobus vista;
-    private ArrayList<Autobuses> autobuses;
+    private ArrayList<Autobuses> lista = new ArrayList<>();
 
     public ControladorAutobus(VistaAutobus vista) {
         this.vista = vista;
@@ -17,7 +18,7 @@ public class ControladorAutobus{
     public void Ejecutar(){
         int op = 0;
         do {            
-            op = vista.Menu();
+            op = vista.menu();
             Casos(op);
         } while (op!=0);
     }
@@ -25,7 +26,7 @@ public class ControladorAutobus{
     public void Casos(int op){
         switch (op) {
             case 1:
-                
+                add(CrearAutobus(vista.IngresarDatos()));
                 break;
             case 2:
                 
@@ -43,6 +44,51 @@ public class ControladorAutobus{
         
         
     }
+    
+    public Autobuses CrearAutobus (String[] datos){
+        Autobuses autobuses = new Autobuses(datos[0] ,Integer.parseInt(datos[1]), datos[2].split(";"));
+        return autobuses;
+    }
+
+    @Override
+    public void add(Autobuses a) {
+        if (buscar(a.getId()) == -1){
+            lista.add(a);
+        }
+    }
+
+    @Override
+    public void delete(String placa) {
+        int indice = buscar(placa);
+        if (indice != -1){
+            lista.remove(lista.get(indice));
+        }
+    }
+
+    @Override
+    public void update(Integer indice, Object usuario) {
+        lista.set((int)indice, (Usuario)usuario);
+    }
+
+    @Override
+    public void toList() {
+        for (Usuario usuario : lista) {
+            System.out.println(usuario.toString());
+        }
+    }
+
+    @Override
+    public int buscar(String id) {
+        int pos=0;
+        for (Autobuses item : lista) {
+            if (item.getId().equals(id)) {
+                return pos;
+            }
+            pos++;
+        }
+        return -1;
+    }
+
     
     
         
