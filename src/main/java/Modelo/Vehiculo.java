2d20255d3531bbcd94dd353d82e77a367683ea00
Equipo Dinamita <sebas@Saeb>
2025-05-29 08:39:25 -0500

    Merge origin/master

--- src/main/java/Modelo/Vehiculo.java
+++ src/main/java/Modelo/Vehiculo.java
@@ -5,5 +5,38 @@ package Modelo;
 public abstract class Vehiculo {
     private String id;
     private int capacidad_maxima;
+    private Itinerario itinerario;
+
+    public Vehiculo(String id, int capacidad_maxima, Itinerario itinerario) {
+        this.id = id;
+        this.capacidad_maxima = capacidad_maxima;
+        this.itinerario = itinerario;
+    }
+    
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getCapacidad_maxima() {
+        return capacidad_maxima;
+    }
+
+    public void setCapacidad_maxima(int capacidad_maxima) {
+        this.capacidad_maxima = capacidad_maxima;
+    }
+
+    public Itinerario getItinerario() {
+        return itinerario;
+    }
+
+    public void setItinerario(Itinerario itinerario) {
+        this.itinerario = itinerario;
+    }
+    
+    
     
 }
