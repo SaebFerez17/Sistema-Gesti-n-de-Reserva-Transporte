ae1ee37d284b6eed7377b61255e34311c0892fec
Familia Ferez <Familia Ferez@DESKTOP-N699CAQ>
2025-06-02 20:41:39 -0500

    Utilidades fecha

---
+++ src/main/java/Utilidades/Fecha.java
@@ -0,0 +1,54 @@
+
+package Utilidades;
+
+
+public class Fecha {
+    
+    private String [] fecha = new String[3];
+    private String [] hora = new String[2];
+
+    public Fecha() {
+    }
+
+    public String[] getFecha() {
+        return fecha;
+    }
+    
+    public String[] getHora() {
+        return hora;
+    }
+
+    public void setFecha(String msj) {
+        String [] partes = msj.split("/");
+        if (partes.length == 3){
+                for (int i = 0; i < 3; i++) {
+                    fecha[i] = partes[i].trim();
+                }
+                this.fecha = partes;
+        }else {
+            System.out.println("Error: Solo ingrese [dd/mm/aa]");
+        }
+    }
+
+
+    public void setHora(String msj) {
+        String [] partes = msj.split(":");
+        if (partes.length == 2){
+            for (int i = 0; i < 2; i++) {
+                hora[i] = partes[i].trim();
+            }
+            this.hora = partes;
+        }else {
+            System.out.println("Error: Solo ingrese [hh:mm]");
+        }    
+    }
+    
+    public void GraficarFecha (){
+        System.out.println(String.join("/", fecha));
+    }
+    
+    public void GraficarHora (){
+        System.out.println(String.join(":", hora));
+    }
+    
+}
